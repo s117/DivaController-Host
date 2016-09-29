551d66a4b805742b24a339b028ae7e8ac33aa1ab
s117 <s117@gs-station.local>
2016-09-30 04:06:32 +0800

    Initial Commit

---
+++ DivaController-Host/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DivaController-Host
+//
+//  Created by s117 on 16/9/30.
+//  Copyright © 2016年 s117. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
